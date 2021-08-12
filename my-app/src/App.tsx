354d43838ca479e24854482749dfa9f60a0c06d7
goldenyak <goldenyakegor@gmail.com>
2021-08-13 00:31:14 +0300

    add some function for buttons

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,11 +7,16 @@ import './App.css';
 
 function App() {
 
-    let [value, setValue] = useState<number>(0)
+    let [calc, setCalc] = useState<string>('')
+
+    function getButtonValue(numberValue: any) {
+       setCalc(calc + numberValue)
+        console.log(calc)
+    }
 
     return (
         <div className={'main_wrapper'}>
-            <input value={value} className={'input'}></input>
+            <input value={calc} className={'input'}></input>
             <div className={'operations_wrapper'}>
                 <button>/</button>
                 <button>*</button>
@@ -19,17 +24,17 @@ function App() {
                 <button>-</button>
             </div>
             <div className={'buttons_wrapper'}>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button>0</button>
-                <button>.</button>
+                <button onClick={ () => getButtonValue(1)}>1</button>
+                <button onClick={ () => getButtonValue(2)}>2</button>
+                <button onClick={ () => getButtonValue(3)}>3</button>
+                <button onClick={ () => getButtonValue(4)}>4</button>
+                <button onClick={ () => getButtonValue(5)}>5</button>
+                <button onClick={ () => getButtonValue(6)}>6</button>
+                <button onClick={ () => getButtonValue(7)}>7</button>
+                <button onClick={ () => getButtonValue(8)}>8</button>
+                <button onClick={ () => getButtonValue(9)}>9</button>
+                <button onClick={ () => getButtonValue(0)}>0</button>
+                <button onClick={ () => getButtonValue('.')}>.</button>
                 <button>=</button>
             </div>
         </div>
