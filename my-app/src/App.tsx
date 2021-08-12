9c74ae297cbe72e1f61fb0ce0cb7bb4683878551
goldenyak <goldenyakegor@gmail.com>
2021-08-13 00:06:11 +0300

    add buttons and styles to App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -12,10 +12,25 @@ function App() {
     return (
         <div className={'main_wrapper'}>
             <input value={value} className={'input'}></input>
-            <div>
+            <div className={'operations_wrapper'}>
+                <button>/</button>
+                <button>*</button>
+                <button>+</button>
+                <button>-</button>
+            </div>
+            <div className={'buttons_wrapper'}>
                 <button>1</button>
                 <button>2</button>
                 <button>3</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>0</button>
+                <button>.</button>
+                <button>=</button>
             </div>
         </div>
     )
